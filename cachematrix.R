d09584fbc316df1f328ad8035d8595baadb76a86
Arun Kumar Aggarwal <aggak63@users.noreply.github.com>
2016-05-06 22:06:57 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inversion using R for inversion of squared matrixes
+## demonstrates use of Lexical Scoping in R
 
-## Write a short comment describing this function
+## For Matrix Inversion in R, first define special vector for matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                  m <- NULL
+                         set <- function(y){
+                                x<<- y
+                                m<<- NULL
+                         }
+                        get <- function () x
+                        setSolve <- function (Solve) m <<- Solve
+                        getSove <- function ()m
+                  
+}
+CacheSolve <- function(x, ...) {
+  ## This function will look up the matrix and cause its inversion
+                cacheSolve <- function(x, ...) {
+                        ## Return a matrix that is the inverse of 'x'
+                        m <- x$getsolve ()
+                                if (!is.null(m)){
+                                        message (" getting cached data")
+                                return (m)
+                                }
+                        data <- x$get
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
